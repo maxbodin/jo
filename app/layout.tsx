2cedf7c0c691321d76ce7b1f66e404e97047971f
Maxime Bodin <maxime.bodin@mailo.com>
2024-10-15 16:50:41 +0200

    layout.tsx : Reformat.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import React from 'react'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-   title: 'Jeux Olympiques',
-   description: 'Classements des Jeux Olympiques',
-}
+  title: "Jeux Olympiques",
+  description: "Classements des Jeux Olympiques"
+};
 
 export default function RootLayout({
-                                      children,
+                                     children
                                    }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <html lang="fr">
-         <body className={inter.className}>
-         {children}
-         </body>
-      </html>
-   )
+  return (
+    <html lang="fr">
+    <body className={inter.className}>
+    {children}
+    </body>
+    </html>
+  );
 }
